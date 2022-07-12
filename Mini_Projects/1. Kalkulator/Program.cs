e05bb9ce6754ce209dd5139f59a1819bf6d93fc2
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-12 19:35:08 +0200

    Pierwszy projekt - Kalkulator

---
+++ Mini_Projects/1. Kalkulator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            char znak;
+            Console.WriteLine("Kalkulator podaj 2 liczby a i b aby je obliczyc oraz sposob w jaki chcesz to zrobic!");
+            Console.WriteLine("Podaj a");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj b");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wybierz operacje: '*' - mnozenie  '/' - dzielenie  '+' - " + "dodawanie  '-' - odejmowanie");
+            znak = Convert.ToChar(Console.ReadLine());
+
+            switch (znak)
+            {
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+                case '+':
+                    Console.WriteLine(a + b);
+                    break;
+                case '/':
+                    if (b != 0)
+                    {
+                        Console.WriteLine(a / b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nie mozna dzielic przez zero! Nie mozna wykonac zadania!");
+                    }
+                    break;
+                case '-':
+                    Console.WriteLine(a - b);
+                    break;
+                default:
+                    Console.WriteLine("Podano niewlasciwy znak! Nie mozna wykonac zadania!");
+                    break;
+            }
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            while (keyInfo.Key != ConsoleKey.Enter)
+                keyInfo = Console.ReadKey();
+        }
+    }
+}
