6cbcff3d7e7285feb0cdc71ca38239cf45d9bd5b
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-13 21:54:05 +0200

    Trzeci projekt - Program używający rekurencji

---
+++ Mini_Projects/3. Program używający rekurencji/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadanie3
+{
+	class Program
+	{
+		static bool pierwsza(int a, int o = 2)
+		{
+			return (o <= Math.Sqrt(a)) ? (a % o == 0) ? false : pierwsza(a, ++o) : true;
+		}
+
+		static void Main(string[] args)
+		{
+			int a;
+			Console.WriteLine("Wpisz liczbe do sprawdzenia czy jest liczba pierwsza");
+			a = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Liczba {0} {1}jest pierwsza", a, (pierwsza(a) ? "" : "nie "));
+			Console.ReadKey();
+		}
+	}
+}
