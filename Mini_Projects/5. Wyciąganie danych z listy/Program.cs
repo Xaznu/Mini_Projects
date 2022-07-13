8a1cfedb4d9496f165c255b53f5c16f5a69e4a35
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-13 22:04:27 +0200

    Piąty projekt - Wyciąganie danych z listy

---
+++ Mini_Projects/5. Wyciąganie danych z listy/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Zadanie5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            //Kolor tekstu dla lepszego wyglądu
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            //Wczytanie pliku tekstowego z imionami
+            var imiona = File.ReadAllLines(@"names.txt");
+
+
+            //Wyświetlenie 246-tego elementu z listy imion
+            void Element()
+            {
+                var index = imiona.ElementAt(246);
+                Console.WriteLine($"Elementem 246 z listy jest: {index}");
+            }
+            //Wyświetlenie osób o imieniu "Anna"
+            void Anna()
+            {
+                Console.WriteLine("\n\nOsoby o imieniu Anna:\n");
+                foreach (var name in imiona)
+                {
+                    if (name.Contains("Anna"))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            }
+            Element();
+            Anna();
+            Console.ReadKey();
+        }
+    }
+}
+
