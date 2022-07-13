f44fac52bf50d549fa5b78136d7f3a02a5e02bd9
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-13 22:08:13 +0200

    Siódmy projekt - Program wyrażenie lambda
    Wyświetlić wynik dla poniższego drzewa z wyrażenia lambda
    
    var a = 10;
    Expression<Func<int>> sum = () => 1 + a + 3 + 4;

---
+++ Mini_Projects/7. Wyrażenie lambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+
+namespace Lambda
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            var a = 10;
+            Expression<Func<int>> sum2 = () => 1 + a + 3 + 4;
+            Func<int> delg3 = sum2.Compile();
+            Console.WriteLine($"Wynik wynosi: {delg3()}");
+        }
+    }
+}
