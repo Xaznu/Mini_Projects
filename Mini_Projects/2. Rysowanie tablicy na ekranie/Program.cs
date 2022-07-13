19c7d049b4812cc1ed7b723e1cbac9a096857349
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-13 21:50:09 +0200

    Drugi projekt projekt - Rysowanie tablicy na ekranie (program wyświetlający tablicę dwuwymiarową o rozmiarze 6x6) 1 ćwiartka - % 2 ćwiartka - # 3 ćwiartka - * 4 ćwiartka - +

---
+++ Mini_Projects/2. Rysowanie tablicy na ekranie/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace tablice
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+            string[,] zadanie = new string[6, 6]
+ {
+
+
+    {"( % )","( % )","( % )","( # )","( # )","( # )",},
+    {"( % )","( % )","( % )","( # )","( # )","( # )",},
+    {"( % )","( % )","( % )","( # )","( # )","( # )",},
+    {"( * )","( * )","( * )","( + )","( + )","( + )",},
+    {"( * )","( * )","( * )","( + )","( + )","( + )",},
+    {"( * )","( * )","( * )","( + )","( + )","( + )",},
+
+ };
+
+            for (int x = 0; x < 6; x++)
+
+            {
+
+                //przerwa pomiedzy znakami
+                Console.WriteLine("");
+
+                for (int y = 0; y < 6; y++)
+
+                {
+                    Console.Write(zadanie[x, y]);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
