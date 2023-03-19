4a63608cf577835a9bbc2fc1db848d77c6d77ad2
Xaznu <krawczynskikewin@gmail.com>
2023-03-19 12:58:56 +0100

    Added warehouse options.

--- Mini_Projects/9. C#App/Program.cs
+++ Mini_Projects/9. C#App/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Warehouse
+{ 
+    class Program
+{
+        //Stała
+        public const string FILE_NAME = "X:\\ImportFile.ods";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to warehouse app!");
+            Console.WriteLine("Please let me know what you want to do:");
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Remove Item");
+            Console.WriteLine("3. Check Item");
+            Console.WriteLine("Press 1, 2, 3...");
+
+            string choice = Console.ReadLine();
+            
+            Console.WriteLine($"You have chosen option number: {choice}");
+
+            //Zmienna
+            int chosenOption = 0;
+
+            Int32.TryParse(choice, out chosenOption);
+
+            Console.WriteLine("Please choose another option...");
+
+            choice = Console.ReadLine();
+
+            Int32.TryParse(choice, out chosenOption);
+        }
+    }
+}
