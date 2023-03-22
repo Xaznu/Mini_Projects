92953149799c79883dd945af652bb450d570c61b
Xaznu <krawczynskikewin@gmail.com>
2023-03-22 21:32:00 +0100

    Class "Helpers" added.

---
+++ Mini_Projects/9. C#App/Helpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._C_App
+{
+    public class Helpers
+    {
+        public int GiveMeNumber()
+        {
+            SomeStructure structure = new SomeStructure();
+        
+
+
+        int number = 5;
+
+            return number;
+        }
+}
+public enum ItemTypes
+{
+    Grocery,
+    Clothing,
+    Electronics
+}
+
+public struct SomeStructure
+{
+    private int numberForStructure;
+    private string nameOfStructure;
+    public SomeStructure(int number, string name)
+    {
+        numberForStructure = number;
+        nameOfStructure = name;
+    }
+        
+    }
+}
+}
